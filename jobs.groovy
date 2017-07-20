18821592067e9dbe5ffecbde96942a3b45222085
uqdalter <uqdalter@CA-SINT-4D5HW22.uq.edu.au>
2017-07-20 14:40:06 +1000

    updated jobs groovy file

--- jobs.groovy
+++ jobs.groovy
@@ -53,7 +53,7 @@ for(system in systems) {
                 }
             }
             steps {
-                shell {"""#!/bin/bash
+                shell ("""#!/bin/bash
 set -axeE
 
 #defining port for selenium #TODO: select free port automatically
@@ -63,19 +63,21 @@ export SELENIUM_PORT=\$(for port in \$(seq 4444 65000); do echo -ne \"\\035\" | 
 sudo docker rm -f selenium-\$SELENIUM_PORT || true
 
 # launching selenium hub docker container
-sudo docker run --name selenium-\$SELENIUM_PORT -d -p \$SELENIUM_PORT:\$SELENIUM_PORT -v /dev/shm:/dev/shm selenium/standalone-chrome:3.4.0-einsteinium 
+sudo docker run --name selenium-\$SELENIUM_PORT -d -p \$SELENIUM_PORT:4444 -v /dev/shm:/dev/shm selenium/standalone-chrome:3.4.0-einsteinium 
 
-#getting docker ip. The test will grab the environment variable
-export SELENIUM_IP=\$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' selenium-\$SELENIUM_PORT)
+# allowing time to start 
+sleep 5
 
 #running the test
-mvn test -Dcucumber.options=\"--tags @${tag}\" -Dlog4j.configuration=file:log4j.properties"""
-                      }
-                shell {"""#!/bin/bash
-$SELENIUM_PORT=`cat env_SELENIUM_PORT.txt`
+mvn test -Dcucumber.options=\"--tags @${tag}\" -Dlog4j.configuration=file:Log4j.xml
+echo \$SELENIUM_PORT > env_SELENIUM_PORT.txt"""
+                      )
+                shell ("""#!/bin/bash
+export SELENIUM_PORT=`cat env_SELENIUM_PORT.txt`
 # cleaning up the container 
 sudo docker rm -f selenium-\$SELENIUM_PORT || true"""
-                      }
+                      )
+            }
              publishers {
                 cucumberReports {
                     jsonReportPath('target/')
