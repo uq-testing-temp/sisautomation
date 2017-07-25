d98f05ddb5a04a7c17134a02554eb5a4f1cfbc2e
uqdalter <uqdalter@CA-SINT-4D5HW22.uq.edu.au>
2017-07-25 14:51:16 +1000

    merged @graduation feature files

--- its-test.bat
+++ its-test.bat
@@ -3,4 +3,4 @@ set arg1=%1
 shift
 shift
 
-mvn clean test -Dcucumber.options="--tags %arg1%" -Dlog4j.configuration=file:Log4j.xml
+mvn clean test -Dcucumber.options="--tags %arg1% --tags ~@skipped" -Dlog4j.configuration=file:Log4j.xml
